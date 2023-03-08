bba61277883d46477c6b2c73b3c1016e32630637
gitahievans <gitahievance@gmail.com>
2023-03-08 10:52:16 +0300

    Uncomment animation code

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("load", function () {
     thankyouState.style.display = "none";
     ratingState.style.display = "block";
-    // gsap.from(rateValues, { duration: 2, opacity: 0, y: 150, stagger: .5 });
-    // gsap.from(submitButton, { duration: 7, opacity: 0})
-    // gsap.from(pleaseNote, {duration: 2, opacity: 0, x: 150})
+    gsap.from(rateValues, { duration: 2, opacity: 0, y: 150, stagger: .5 });
+    gsap.from(submitButton, { duration: 7, opacity: 0})
+    gsap.from(pleaseNote, {duration: 2, opacity: 0, x: 150})
 
-    // gsap.from(logo, {duration: 3, xPercent: -50, rotateX:360, delay: 3})
-    // gsap.from(logo, { duration: 3, xPercent: 50, ease: "bounce.out" });
+    gsap.from(logo, {duration: 3, xPercent: -50, rotateX:360, delay: 3})
+    gsap.from(logo, { duration: 3, xPercent: 50, ease: "bounce.out" });
   });
   
 
@@ -50,10 +50,10 @@ document.addEventListener("DOMContentLoaded", () => {
       if (rating) {
         ratingResult.textContent = `You selected ${rating} out of 5`;
       }
-      //  gsap.from(thankNote, { duration: 2, opacity: 0, y: 150 });
-      //  gsap.from(".top img", { duration: 3, opacity: 0, y: -150, rotateX: 360, ease: "easeInEaseOut", delay: 2.5});
-      //  gsap.from(".results", { duration: 3, z: 150, opacity: 0})
-      //  gsap.from(".thankyou section h1", { duration: 2, rotateY: 360, x: 350, opacity: 0, ease: "easeOut"})
+       gsap.from(thankNote, { duration: 2, opacity: 0, y: 150 });
+       gsap.from(".top img", { duration: 3, opacity: 0, y: -150, rotateX: 360, ease: "easeInEaseOut", delay: 2.5});
+       gsap.from(".results", { duration: 3, z: 150, opacity: 0})
+       gsap.from(".thankyou section h1", { duration: 2, rotateY: 360, x: 350, opacity: 0, ease: "easeOut"})
     });
   };
   submitRating();
